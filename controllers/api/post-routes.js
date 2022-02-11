fc26ad28d5ada6c2c2b284bc42247d36ff0a89c2
Charlie H <charltonhua@gmail.com>
2022-02-11 00:14:01 -0800

    comment model /route, revise user and post routes for comment

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 router.get('/', (req, res) => {
   Post.findAll({
+    order: [['created_at', 'DESC']],
     attributes: [
       'id',
       'post_url',
       'title',
       'created_at',
-      // reference column, used to display number of votes on post
       [
         sequelize.literal(
           '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
@@ -17,10 +17,17 @@ router.get('/', (req, res) => {
         'vote_count',
       ],
     ],
-
-    order: [['created_at', 'DESC']],
     include: [
       {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+      {
+        model: User,
         attributes: ['username'],
       },
     ],
