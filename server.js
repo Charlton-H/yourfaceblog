cc48fc8f037b28b2728c7d704a1533b0b4502e83
Charlie H <charltonhua@gmail.com>
2022-02-20 02:53:48 -0800

    complete log in and log out w/ handlebars

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+// library allows us to connect to the back end
+const session = require('express-session');
+// library automatically stores the sessions
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,6 +32,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+
+app.use(session(sess));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
